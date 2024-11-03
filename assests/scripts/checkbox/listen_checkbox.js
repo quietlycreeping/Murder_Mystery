8cc2b5e8490758b5904b33a4f85bb1bb61ba9e5c
quietlycreeping <60121681+quietlycreeping@users.noreply.github.com>
2024-11-03 15:23:57 -0500

    newfiles

--- assests/scripts/checkbox/listen_checkbox.js
+++
@@ -1,5 +0,0 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-checkboxes.forEach((checkbox) => 
-    {
-        checkbox.addEventListener('change', saveCheckboxStates);
-    });
